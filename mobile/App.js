51e073be0cb6e34aea01d01c14e20476474aa332
MardanKasym <fhefeuegfbsceh@gmail.com>
2026-02-13 15:58:51 +0500

    Create App.js for React Native mobile application to handle navigation

---
+++ mobile/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/Home';
+import MenuScreen from './screens/Menu';
+import CartScreen from './screens/Cart';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Menu" component={MenuScreen} />
+        <Stack.Screen name="Cart" component={CartScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
