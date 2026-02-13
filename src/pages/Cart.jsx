c706f615867539acc1d52379921270b3bfc9348e
MardanKasym <fhefeuegfbsceh@gmail.com>
2026-02-13 15:34:24 +0500

    Create Cart component for Smart Canteen app

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div>
+            <h1>Your Shopping Cart</h1>
+            <p>Items will be displayed here.</p>
+            {/* Add cart items component or logic here */}
+        </div>
+    );
+};
+
+export default Cart;
